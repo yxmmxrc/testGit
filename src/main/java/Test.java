a0f5e5b56c63a9ce4acf23e80c32be289bf048f3
cai <8888@888.yt>
2020-04-11 16:06:07 +0800

    test class

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -8,6 +8,12 @@
 public class Test {
     public static void main(String[] args) {
         System.out.println("master");
+        System.out.println("master");
+        System.out.println("master");
+        System.out.println("master");
+        System.out.println("master");
+        System.out.println("master");
+
     }
 
 }
