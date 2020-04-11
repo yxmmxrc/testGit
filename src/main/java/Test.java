b7054faf5f1f6c9c8b0597e0c5d3044cce4b0ed0
cai <8888@888.yt>
2020-04-11 15:44:25 +0800

    test class

---
+++ src/main/java/Test.java
@@ -0,0 +1,13 @@
+/**
+ * @Author: 袭荣才 Xi rong cai
+ * @User:Administrator
+ * @Date:2020-04-11
+ * @Time:15:27
+ * @Name:Test
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("master");
+    }
+
+}
